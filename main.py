803fa7583ab1d0b534857306c9adbdaa57538332
Daniel <danielmorernoinf@gmail.com>
2024-04-03 21:10:46 -0300

    se sube prueba final

---
+++ main.py
@@ -0,0 +1,14 @@
+import templates
+from api_get import request_get
+
+url = "https://aves.ninjas.cl/api/birds" 
+response = request_get(url)
+
+cadena = ""
+
+for dicc in response:
+    cadena = cadena + templates.template2.substitute(titulo_esp= dicc["name"]["spanish"], titulo_en=dicc["name"]["english"], url_img_full=dicc["images"]["full"])
+
+file = open("AvesChile.html", "w")
+message = templates.template.substitute(contenido= cadena)
+file.write(message)
